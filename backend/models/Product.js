f9ca68d8961d46cca8b7b8874143af8cfa398a58
atharv pawar <atharvp721@gmail.com>
2026-02-25 02:03:24 +0530

    login profile routes

---
+++ backend/models/Product.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  discountPrice: {
+  type: Number,
+},
+
+countInStock: {
+  type: Number,
+  required: true,
+  default: 0,
+},
+
+sku: {
+  type: String,
+  unique: true,
+  required: true,
+},
+
+category: {
+  type: String,
+  required: true,
+},
+});
--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-
+const protect=require("../middleware/authMiddleware");
 const router = express.Router();
 
 // POST /api/users/register
@@ -58,6 +58,64 @@ router.post("/register", async (req, res) => {
     console.log(error);
     res.status(500).send("Server Error");
   }
+});
+
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    // Check password
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    
+    const payload = {
+      user: {
+        id: user._id,
+        role: user.role,
+      },
+    };
+
+    
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        res.status(200).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+
+router.get("/profile", protect, async (req, res) => {
+  res.json(req.user);
 });
 
 module.exports = router;
